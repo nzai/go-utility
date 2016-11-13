e773e912f208cf30ade4ac54a8ad52179895c31b
nzai <nzai@16.3com>
2016-11-14 01:33:57 +0800

    [Format]rename

--- db/sqlite/sqlite.go
+++ db/sqlite/sqlite.go
@@ -2,8 +2,8 @@ package sqlite
 
 import "database/sql"
 
-// IsExists 判断表或者索引是否存在
-func IsExists(db *sql.DB, name string) (bool, error) {
+// TableOrIndexExists 判断表或者索引是否存在
+func TableOrIndexExists(db *sql.DB, name string) (bool, error) {
 
 	stmt, err := db.Prepare("SELECT name FROM sqlite_master WHERE type IN ('table', 'index') and name=?")
 	if err != nil {
